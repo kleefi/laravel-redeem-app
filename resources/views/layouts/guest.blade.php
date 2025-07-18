df440d22933368ddaf147796cde314a882dc3320
kleefi <decharvi.charles@gmail.com>
2025-07-18 10:23:44 +0700

    fixing login/register ui

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title') - {{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -17,15 +17,23 @@
 </head>
 
 <body class="font-sans text-gray-900 antialiased">
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div>
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
+    <div class="grid md:grid-cols-2 grid-cols-1 items-center">
+        <div class="md:h-screen hidden md:flex items-center justify-center bg-cover bg-center"
+            style="background-image: url('{{ asset('banner.avif') }}')">
+            <div
+                class="flex items-center w-[80%] flex-col leading-5 justify-center text-center bg-orange-100/90 p-8 rounded-md border border-[#1b1b1b]">
+                <h2 class="text-2xl mb-2">"Day one will become one day"</h2>
+                <p>— just keep going. Slow progress is still progress. Do it until your 'day one' becomes the day you
+                    succeed. — </p>
+            </div>
         </div>
-
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-            {{ $slot }}
+        <div class="w-[70%] mx-auto md:block md:h-auto h-screen flex items-center justify-center">
+            <div>
+                <a href="#" class="flex justify-center text-center space-x-8 mb-8">
+                    <span class="text-2xl font-bold text-gray-500">LOGO</span>
+                </a>
+                {{ $slot }}
+            </div>
         </div>
     </div>
 </body>
