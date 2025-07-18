702c0e06901b8b43a233ebaa0dc1c909d059babf
kleefi <decharvi.charles@gmail.com>
2025-07-18 09:24:01 +0700

    add toast contact

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Contact extends Model
 {
+    public $incrementing = false;
+    protected $keyType = 'string';
     protected $fillable = ["name", "category", "email", "phone", "message"];
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->id = (string) Str::uuid();
+        });
+    }
 }
