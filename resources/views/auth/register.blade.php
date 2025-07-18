002637f305f5da8a71915cb130ea98623c546295
kleefi <decharvi.charles@gmail.com>
2025-07-18 11:59:22 +0700

    register flash message

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -41,6 +41,9 @@ Register
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
+        @if(session('success'))
+        <p class="p-3 mt-2 text-gray-800 bg-green-400 rounded-sm">{{ session('success') }}</p>
+        @endsession
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 href="{{ route('login') }}">
